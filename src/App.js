2343ecf317796cc8470fbb6e5b48a82efd1c63ab
iblearner <kienvi96@hotmail.com>
2021-01-11 20:18:18 +1000

    Created a hand class to represent a players hand.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
 import Piece from './components/Piece';
+import Hand from "./components/Hand"
+import data from "./card-data.json";
 
 function App() {
   return (
     <div className="App">
         hi
-        <Piece/>
+        <Piece />
+        <Hand hand={data.cards}/>
     </div>
   );
 }
---
+++ src/components/Hand.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Piece from "./Piece"
+
+class Hand extends Component {
+    state = {  }
+
+    mapCards = () => {
+        const mappedHand = this.props.hand.map(single => {
+            return <Piece cardData={single}/>
+        })
+        return mappedHand
+    }
+
+    render() { 
+        return (
+            <>
+            {this.mapCards()}
+            </>
+        );
+    }
+}
+ 
+export default Hand;
