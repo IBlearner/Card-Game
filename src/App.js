ceb32de29c4d16818e13e504d45488c36e642959
iblearner <kienvi96@hotmail.com>
2021-01-11 19:56:32 +1000

    Created a piece class to represent a card, with a stock card sample.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+import Piece from './components/Piece';
 
 function App() {
   return (
     <div className="App">
         hi
+        <Piece/>
     </div>
   );
 }
